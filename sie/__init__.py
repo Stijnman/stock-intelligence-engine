0b619f0bc58949481db4d2eea4332327c4f376fa
Stijnman <31705123+Stijnman@users.noreply.github.com>
2026-07-25 02:23:11 +0000

    chore: Export forecast_narrative_phase in package init (v2.7.0)

--- sie/__init__.py
+++ sie/__init__.py
@@ -1,5 +1,11 @@
 """Stock Intelligence Engine core package."""
 from sie.analyzer import analyze_watchlist, run_report
-from sie.social import scan_viral_sentiment, integrate_social_to_row
+from sie.social import scan_narrative_intelligence, integrate_social_to_row, forecast_narrative_phase
 
-__all__ = ["analyze_watchlist", "run_report", "scan_viral_sentiment", "integrate_social_to_row"]
+__all__ = [
+    "analyze_watchlist",
+    "run_report",
+    "scan_narrative_intelligence",
+    "integrate_social_to_row",
+    "forecast_narrative_phase",
+]
